931c73fb08da1f3e3f83a6347161472e1acc9f77
Pettecco <pettersonibs@gmail.com>
2024-02-23 18:04:47 -0300

    commit

---
+++ ZeDoLanche_Ex1/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int lanche_1 = 10;
+        int lanche_2 = 4;
+        int lanche_3 = 2;
+        int total = lanche_1 + lanche_2 + lanche_3;
+        float media = total/3.0f;
+
+        System.out.println("O total de lanches foi: " + total);
+        System.out.println("A média de lanches consumidos foi de: " + media);
+    }
+}
