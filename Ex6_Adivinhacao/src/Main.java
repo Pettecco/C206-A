c6f0550de51d1136edc0ac8dfafc1697c7062451
Pettecco <pettersonibs@gmail.com>
2024-03-02 07:50:11 -0300

    commit

---
+++ Ex6_Adivinhacao/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        Random rand = new Random();
+        int numAdivinhado;
+        int numGerado = rand.nextInt(10) + 1;
+
+        do {
+            System.out.println("Qual o número estou pensando?");
+            numAdivinhado = entrada.nextInt();
+
+            if(numAdivinhado > numGerado){
+                System.out.println("Estou pensando em um número menor");
+            } else if(numAdivinhado < numGerado) {
+                System.out.println("Estou pensando em um número maior");
+            }
+        } while (numGerado != numAdivinhado);
+
+        System.out.println("Parabéns! Voce acertou o numero!");
+        entrada.close();
+    }
+}
