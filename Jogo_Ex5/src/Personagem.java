50066fa8277daed8ca369161064de3d531d3712f
Pettecco <pettersonibs@gmail.com>
2024-03-14 18:53:48 -0300

    commit

--- Jogo_Ex5/src/Personagem.java
+++ Jogo_Ex5/src/Personagem.java
@@ -5,10 +5,22 @@ public class Personagem {
 
     void usarArmas(){
         System.out.println(this.nome + " usando a arma: " + this.arma.nome);
+        if(arma.resistencia > 2){
+            this.arma.resistencia = arma.resistencia - 2;
+        } else {
+            this.arma.resistencia = 0;
+        }
+        System.out.println(this.nome + " usou a arma: " + this.arma.nome + "e agora ela tem a resistencia: " + arma.resistencia);
     }
 
     void tomarDano(){
-        System.out.println(this.nome + " tem " + this.pontos);
+        System.out.println(this.nome + " tem isso de vida: " + this.pontos);
         System.out.println(this.nome + " tomou dano e seus pontos de vida abaixaram");
+        if(pontos > 5){
+            this.pontos = pontos - 5;
+        } else {
+            this.pontos = 0;
+        }
+        System.out.println(this.nome + " tem isso de vida: " + this.pontos);
     }
 }
